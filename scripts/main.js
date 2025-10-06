e1347027d31787978b8c351e9ea2f450184b6cfd
gabrieltosiaudio <gabrielsiqueiratosi@gmail.com>
2025-10-06 18:03:22 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+
+// Mobile menu toggle
+document.getElementById('mobileBtn').addEventListener('click', function(){
+  var m = document.getElementById('mobileMenu');
+  m.classList.toggle('hidden');
+});
